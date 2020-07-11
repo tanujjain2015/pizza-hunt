f19105929b6b870c89bdf4682394ce983d27e393
tanjain <tanuj.jain@oracle.com>
2020-07-10 19:16:05 -0700

    Final chnages

---
+++ models/Comment.js
@@ -0,0 +1,60 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReplySchema = new Schema(
+  {
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String,
+      required: true
+    },
+    writtenBy: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+  replies: [ReplySchema]
+  },
+  {
+    toJSON: {
+      virtual: true,
+      getters: true
+    }
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
+
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
